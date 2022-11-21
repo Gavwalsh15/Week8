2e650a8a170fd7d7eef92a148124a2729e7bac2e
G00402296 <G00402296@atu.ie>
2022-11-21 09:44:57 +0000

    Added append

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -2,11 +2,26 @@ package ie.atu.week8;
 
 import java.io.*;
 
-public class CreateFile {
-        public static void main(String[] args)
-        {
+public class CreateFile
+{
+    public static void main(String[] args)
+    {
             File myFile = new File("MyFile.txt");
             System.out.println("My file is located at" + myFile.getAbsolutePath());
-        }
 
+            try
+            {
+                FileWriter outputFile = new FileWriter(myFile, true);
+                outputFile.write("Gavin Test 2");
+                outputFile.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+
+
+
+
+    }
 }
