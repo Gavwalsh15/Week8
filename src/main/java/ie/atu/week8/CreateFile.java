ae42f06db39885a227931bcc9fc8f874f753209c
G00402296 <G00402296@atu.ie>
2022-11-21 10:55:32 +0000

    Added  prompt the user to enter a filename

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -1,22 +1,35 @@
 package ie.atu.week8;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class CreateFile
 {
     public static void main(String[] args) {
-        File myFile = new File("MyFile.txt");
-        System.out.println("My file is located at" + myFile.getAbsolutePath());
 
+        File File1 = new File("File1.txt");
+        System.out.println("My file is located at" + File1.getAbsolutePath());
+        Scanner inputFile = null;
         try {
-            FileWriter myWriter = new FileWriter(myFile, true);
+            FileWriter myWriter = new FileWriter(File1, true);
             PrintWriter myPrinter = new PrintWriter(myWriter);
-            myPrinter.println("Gavin1");
+            myPrinter.println("Hi Paul How Are You");
             myPrinter.close();
+
+
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Enter the filename: ");
+            String filename = keyboard.nextLine();
+            inputFile = new Scanner(new File(filename));
+
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-
+        while (inputFile.hasNext())
+        {
+            String str = inputFile.nextLine();
+            System.out.println(str);
+        }
     }
 }
