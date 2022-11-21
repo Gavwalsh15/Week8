4f7147e0852c1bf42e7cf61c23a8a3412897c217
G00402296 <G00402296@atu.ie>
2022-11-21 09:30:27 +0000

    added print + file location

--- src/main/java/ie/atu/week8/CreateFile.java
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -6,6 +6,7 @@ public class CreateFile {
         public static void main(String[] args)
         {
             File myFile = new File("MyFile.txt");
+            System.out.println("My file is located at" + myFile.getAbsolutePath());
         }
 
 }
