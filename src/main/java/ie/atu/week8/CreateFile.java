7a01403ae9eb929a63f232f78d7c9e30ba8b49bd
G00402296 <G00402296@atu.ie>
2022-11-21 09:28:49 +0000

    Commit first Code lines

---
+++ src/main/java/ie/atu/week8/CreateFile.java
@@ -0,0 +1,11 @@
+package ie.atu.week8;
+
+import java.io.*;
+
+public class CreateFile {
+        public static void main(String[] args)
+        {
+            File myFile = new File("MyFile.txt");
+        }
+
+}
